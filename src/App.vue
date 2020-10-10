26e75c2c5c07d564f32d1a3a2e846e7547c9175c
zccon <1297956208@qq.com>
2020-10-10 17:03:41 +0800

    自定义下拉框组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>自定义下拉框组件</h1>
+    <div class="mainDiv">
+      <Selects :list='letter' class="childDiv" @checkItem = "letterchecked"></Selects>
+      <Selects :list='number' class="childDiv" @checkItem = "numberchecked"></Selects>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//组件的引入
+import Selects from './components/Selects.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      letter:['a','b','c','d','e'],
+      number:[1,2,3,4,5,6,7,8,9]
+    }
+  },
+  components: {//组件的声明
+    Selects
+  },
+  methods:{
+    //输出选择的项
+    letterchecked:function(index){
+      console.log('下拉框1选中项：{index:'+index+','+'value:'+this.letter[index]+'}')
+    },
+    numberchecked:function(index){
+      console.log('下拉框2选中项：{index:'+index+','+'value:'+this.number[index]+'}')
+    }
   }
 }
 </script>
@@ -24,5 +44,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+/* 父组件布局 */
+.mainDiv {
+    margin-top: 2%;
+}
+.childDiv{
+    float: left;
 }
 </style>
